828a7dc30e8dd2dc84c415a39c183cfd5cde1163
mgalcudia <malcudia@gmail.com>
2015-01-02 00:47:25 +0100

    Inicio del modelo para usuarios

---
+++ app/models/modeloUsuario.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Modelo para los usuarios
+ */
+require Raiz . '\db.php';
+class ModeloUsuarios{
+	
+	private $bd;
+	
+	
+	function __construct(){
+		
+		$this->bd= Db::getInstance();
+	}
+	
+	
+	
+	
+	
+	
+}
